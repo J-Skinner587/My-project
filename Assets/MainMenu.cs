e40b8ede2b45dccbaa8ae18a888781bdaa8f3b68
J-Skinner587 <93839680+J-Skinner587@users.noreply.github.com>
2022-10-22 23:17:47 +0100

    Menus

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -1,18 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void PlayGame()
     {
-        
+        SceneManager.LoadScene("Game");
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Restart()
     {
-        
+        SceneManager.LoadScene("Game");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("QUIT");
+        Application.Quit();
+    }
+
+    public void Back()
+    { 
+        SceneManager.LoadScene("Menus");
     }
 }
